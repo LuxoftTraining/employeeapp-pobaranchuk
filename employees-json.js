9f0abb84684ae72e0b27f4864316e492167c808b
pobaranchuk <you@example.com>
2023-01-19 14:35:25 +0100

    Added my files

---
+++ employees-json.js
@@ -0,0 +1,35 @@
+const DATA = {
+
+    employees: [
+        {
+            id: 1,
+            name: "Peter",
+            surmane: "Peterson",
+            department: "IT"
+        },
+        {
+            id: 2,
+            name: "John",
+            surmane: "Dohn",
+            department: "HR"
+        },
+        {
+            id: 3,
+            name: "Ala",
+            surmane: "Flask",
+            department: "PR"
+        },
+        {
+            id: 4,
+            name: "Viktor",
+            surmane: "Rutenberg",
+            department: "IT"
+        },
+        {
+            id: 5,
+            name: "Kris",
+            surmane: "Ales",
+            department: "PR"
+        }
+    ]
+}
